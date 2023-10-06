3a8b0e3e4407d759d6f9f3e18fed7604737b08e8
Dani F <114934334+Yuf0ria@users.noreply.github.com>
2023-10-06 08:41:26 +0800

    ;

--- Assets/PlayerFire.cs
+++ Assets/PlayerFire.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
+    public Transform spawnBulletHere;
+    public GameObject bulletPrefab;
+    //float Variable of speed bullet
+    public float bulletSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,17 @@ public class PlayerFire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))//left mouse button is clicked
+        {
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, spawnBulletHere.position, spawnBulletHere.rotation);
+        Rigidbody bulletRB = bullet.GetComponent<Rigidbody>();
+
+        bulletRB.AddForce(spawnBulletHere.forward * bulletSpeed, ForceMode.Impulse);
     }
 }
